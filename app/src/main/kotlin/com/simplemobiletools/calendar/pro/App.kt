dc5d6c1e678f905d6d46474de696824e50100782
Andrii Bodnar <andrii.bodnar@crowdin.com>
2020-01-09 15:58:45 +0200

    Implement Crowdin SDK for localization

--- app/src/main/kotlin/com/simplemobiletools/calendar/pro/App.kt
+++ app/src/main/kotlin/com/simplemobiletools/calendar/pro/App.kt
@@ -1,11 +1,21 @@
 package com.simplemobiletools.calendar.pro
 
 import androidx.multidex.MultiDexApplication
+import com.crowdin.platform.Crowdin
+import com.crowdin.platform.CrowdinConfig
 import com.simplemobiletools.commons.extensions.checkUseEnglish
 
 class App : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
+
         checkUseEnglish()
+
+        Crowdin.init(
+            applicationContext,
+            CrowdinConfig.Builder()
+                .withDistributionHash("2f1ba0f58f6e7146fa8e9b7uo3a")
+                .build()
+        )
     }
 }
